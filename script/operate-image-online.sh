90819847761b94403209d9f29fbec338534483b9
chenguanming <2554550408@qq.com>
2023-02-16 15:38:00 +0800

    修改操作镜像的脚本，线上拉取和离线导出

--- script/operate-image-online.sh
+++ script/operate-image-online.sh
@@ -72,73 +72,8 @@ docker tag registry.cn-hangzhou.aliyuncs.com/feng888/gcr.io:eventing-cmd-mtping-
 docker tag registry.cn-hangzhou.aliyuncs.com/feng888/gcr.io:net-istio-cmd-controller-sha256-f253b82941c2 gcr.io/knative-releases/knative.dev/net-istio/cmd/controller:sha256-f253b82941c2
 docker tag registry.cn-hangzhou.aliyuncs.com/feng888/gcr.io:net-istio-cmd-webhook-sha256-a705c1ea8e9e gcr.io/knative-releases/knative.dev/net-istio/cmd/webhook:sha256-a705c1ea8e9e   
 
-
-
-# 一次性导出
-docker save -o ./kf-manifests-1.6.1-gcrio.tar ghcr.io/dexidp/dex:v2.31.2 \
-gcr.io/tfx-oss-public/ml_metadata_store_server:1.5.0 \
-gcr.io/kubebuilder/kube-rbac-proxy:v0.8.0 \
-gcr.io/arrikto/istio/pilot:1.14.1-1-g19df463bb \
-gcr.io/arrikto/kubeflow/oidc-authservice:28c59ef \
-gcr.io/ml-pipeline/cache-server:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/metadata-envoy:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/metadata-writer:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/minio:RELEASE.2019-08-14T20-37-41Z-license-compliance \
-gcr.io/ml-pipeline/api-server:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/persistenceagent:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/scheduledworkflow:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/frontend:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/viewer-crd-controller:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/visualization-server:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/mysql:5.7-debian \
-gcr.io/ml-pipeline/workflow-controller:v3.3.8-license-compliance \
-gcr.io/knative-releases/knative.dev/serving/cmd/activator:sha256-93ff6e693577 \
-gcr.io/knative-releases/knative.dev/serving/cmd/autoscaler:sha256-007820fdb75b \
-gcr.io/knative-releases/knative.dev/serving/cmd/domain-mapping:sha256-23baa1932232 \
-gcr.io/knative-releases/knative.dev/serving/cmd/domain-mapping-webhook:sha256-847bb97e3844 \
-gcr.io/knative-releases/knative.dev/serving/cmd/webhook:sha256-9084ea8498ea \
-gcr.io/knative-releases/knative.dev/serving/cmd/controller:sha256-75cfdcfa050a \
-gcr.io/knative-releases/knative.dev/serving/cmd/queue:sha256-14415b204ea8 \
-gcr.io/knative-releases/knative.dev/eventing/cmd/controller:sha256-dc0ac2d8f235 \
-gcr.io/knative-releases/knative.dev/eventing/cmd/webhook:sha256-b7faf7d253bd \
-gcr.io/knative-releases/knative.dev/eventing/cmd/mtping:sha256-632d9d710d07 \
-gcr.io/knative-releases/knative.dev/net-istio/cmd/controller:sha256-f253b82941c2 \
-gcr.io/knative-releases/knative.dev/net-istio/cmd/webhook:sha256-a705c1ea8e9e
-
-
-
 # 删除
-docker rmi ghcr.io/dexidp/dex:v2.31.2 \
-gcr.io/tfx-oss-public/ml_metadata_store_server:1.5.0 \
-gcr.io/kubebuilder/kube-rbac-proxy:v0.8.0 \
-gcr.io/arrikto/istio/pilot:1.14.1-1-g19df463bb \
-gcr.io/arrikto/kubeflow/oidc-authservice:28c59ef \
-gcr.io/ml-pipeline/cache-server:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/metadata-envoy:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/metadata-writer:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/minio:RELEASE.2019-08-14T20-37-41Z-license-compliance \
-gcr.io/ml-pipeline/api-server:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/persistenceagent:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/scheduledworkflow:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/frontend:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/viewer-crd-controller:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/visualization-server:2.0.0-alpha.5 \
-gcr.io/ml-pipeline/mysql:5.7-debian \
-gcr.io/ml-pipeline/workflow-controller:v3.3.8-license-compliance \
-gcr.io/knative-releases/knative.dev/serving/cmd/activator:sha256-93ff6e693577 \
-gcr.io/knative-releases/knative.dev/serving/cmd/autoscaler:sha256-007820fdb75b \
-gcr.io/knative-releases/knative.dev/serving/cmd/domain-mapping:sha256-23baa1932232 \
-gcr.io/knative-releases/knative.dev/serving/cmd/domain-mapping-webhook:sha256-847bb97e3844 \
-gcr.io/knative-releases/knative.dev/serving/cmd/webhook:sha256-9084ea8498ea \
-gcr.io/knative-releases/knative.dev/serving/cmd/controller:sha256-75cfdcfa050a \
-gcr.io/knative-releases/knative.dev/serving/cmd/queue:sha256-14415b204ea8 \
-gcr.io/knative-releases/knative.dev/eventing/cmd/controller:sha256-dc0ac2d8f235 \
-gcr.io/knative-releases/knative.dev/eventing/cmd/webhook:sha256-b7faf7d253bd \
-gcr.io/knative-releases/knative.dev/eventing/cmd/mtping:sha256-632d9d710d07 \
-gcr.io/knative-releases/knative.dev/net-istio/cmd/controller:sha256-f253b82941c2 \
-gcr.io/knative-releases/knative.dev/net-istio/cmd/webhook:sha256-a705c1ea8e9e
 
-# 
 docker rmi registry.cn-hangzhou.aliyuncs.com/feng888/gcr.io:dex-v2.31.2 \
 registry.cn-hangzhou.aliyuncs.com/feng888/gcr.io:ml_metadata_store_server-1.5.0 \
 registry.cn-hangzhou.aliyuncs.com/feng888/gcr.io:kube-rbac-proxy-v0.8.0 \
@@ -170,11 +105,6 @@ registry.cn-hangzhou.aliyuncs.com/feng888/gcr.io:net-istio-cmd-controller-sha256
 registry.cn-hangzhou.aliyuncs.com/feng888/gcr.io:net-istio-cmd-webhook-sha256-a705c1ea8e9e
 
 
-# 一次性导入
-docker load -i ./kf-manifests-1.6.1-gcrio.tar
 
-
-
-## 其他镜像
 
 
